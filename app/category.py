c936118b21b0cf8cb41c078df0f3a1d0ca619e74
Manuel Pereira <afonso.pereira4525@gmail.com>
2023-02-18 01:34:06 +0000

    Adds categories

--- app/category.py
+++ app/category.py
@@ -7,8 +7,8 @@ class BaseCategory:
         self.canvas = canvas
         self.color = color
         self._sim_category = probability
-        self.item = self.canvas.create_text(self.x, self.y, anchor="n",fill=self.color,text= f"{self.name} ({int(self.sim_category*100)} %) ({int(self.x),int(self.y)})",font=("Purisa", 14))
-        self.fence = self.canvas.create_oval(self.x-20, self.y-20, self.x+20, self.y+20, outline=self.color, width=3)
+        self.item = self.canvas.create_text(self.x, self.y, anchor="n",fill=self.color,text= f"{self.name} ({int(self.sim_category*100)} %)",font=("Purisa", 14))
+        #self.fence = self.canvas.create_oval(self.x-20, self.y-20, self.x+20, self.y+20, outline=self.color, width=3)
     
     def draw(self):
         pass
@@ -19,11 +19,15 @@ class BaseCategory:
     y = property(lambda self: self._y,None,None,"Gets y")
     def _set_sim_category(self, probability):
         self._sim_category = probability
-        self.canvas.itemconfig(self.item, text= f"{self.name} ({int(self.sim_category*100)} %) ({int(self.x),int(self.y)})")
+        self.canvas.itemconfig(self.item, text= f"{self.name} ({int(self.sim_category*100)} %)")
     @classmethod
-    def colors(cls):
-        #generate dark colors
-        return ["navy","teal","aqua","red","green","blue","orange","purple","pink","brown","black","white","grey","cyan","magenta","gold","torquoise","olive","maroon"]
+    def colors(cls,i=None):
+        colors = ["navy","teal","aqua","red","green","blue","orange","purple","pink","brown","black","grey","cyan","magenta","gold","torquoise","olive","maroon"]
+
+        if i is not None:
+            return colors[i%len(colors)]
+        else:
+            return colors
         #add 20 different colors
         return ["navy","teal","aqua","red","green","blue","yellow","orange","purple","pink","brown","black","white","grey","cyan","magenta","gold","torquoise","olive","maroon"]
     name = property(lambda self: self._name,None,None,"Gets name")
